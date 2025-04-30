1247e0dc972caae3018004f5f1e595784fdb074e
lihao <lihao@neolix.cn>
2025-04-30 15:02:17 +0800

    update initsql

--- app/initsql/odyssey_2025-04-30_145919.sql
+++ app/initsql/odyssey_2025-04-30_145919.sql
@@ -28,24 +28,18 @@ DROP TABLE IF EXISTS `build_records`;
 CREATE TABLE `build_records` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '记录唯一标识，自增',
   `name` varchar(255) NOT NULL COMMENT '构建记录名称',
-  `project_name` varchar(255) DEFAULT NULL COMMENT '构建所属项目',
-  `env` varchar(255) DEFAULT NULL COMMENT '构建的环境',
+  `project_name` varchar(255) NOT NULL COMMENT '构建所属项目',
+  `env` varchar(255) NOT NULL COMMENT '构建的环境',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
   `build_user` varchar(255) DEFAULT NULL COMMENT '谁构建的',
-  `tag` varchar(255) DEFAULT NULL COMMENT '构建的标签(测试, 预发, 上线)',
+  `tag` varchar(255) NOT NULL COMMENT '构建的标签(测试, 预发, 上线)',
   `description` text COMMENT '构建记录的描述',
-  `status` varchar(255) DEFAULT NULL COMMENT '构建状态(Success, Pending, Failed)',
+  `status` varchar(255) NOT NULL COMMENT '构建状态(Success, Pending, Failed)',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录最后更新时间',
   PRIMARY KEY (`id`) COMMENT '主键：记录唯一标识',
   UNIQUE KEY `name` (`name`,`project_name`) COMMENT '唯一索引：构建记录名称和项目名称组合唯一'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='构建记录表，存储每次构建的相关信息';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `build_records`
---
-
-/*!40000 ALTER TABLE `build_records` DISABLE KEYS */;
-/*!40000 ALTER TABLE `build_records` ENABLE KEYS */;
 
 --
 -- Table structure for table `build_service_records`
@@ -57,15 +51,16 @@ DROP TABLE IF EXISTS `build_service_records`;
 CREATE TABLE `build_service_records` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '记录唯一标识，自增',
   `service_name` varchar(255) NOT NULL COMMENT '服务名称',
-  `project_name` varchar(255) DEFAULT NULL COMMENT '服务所属项目',
-  `env` varchar(255) DEFAULT NULL COMMENT '服务构建环境',
+  `project_name` varchar(255) NOT NULL COMMENT '服务所属项目',
+  `env` varchar(255) NOT NULL COMMENT '服务构建环境',
   `image` varchar(255) DEFAULT NULL COMMENT '构建出来的镜像',
-  `build_record_name` varchar(255) DEFAULT NULL COMMENT '服务所属的构建记录名称',
+  `build_record_name` varchar(255) NOT NULL COMMENT '服务所属的构建记录名称',
   `build_url` text COMMENT '构建的 Jenkins URL',
-  `status` varchar(255) DEFAULT 'pending' COMMENT '服务当前构建状态(Success, Pending, Failed, Abort)',
+  `status` varchar(255) NOT NULL DEFAULT 'pending' COMMENT '服务当前构建状态(Success, Pending, Failed, Abort)',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
-  `branch` varchar(255) DEFAULT NULL COMMENT '构建分支',
+  `branch` varchar(255) NOT NULL COMMENT '构建分支',
   `build_id` bigint DEFAULT '0' COMMENT 'Jenkins 构建 ID',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录最后更新时间',
   PRIMARY KEY (`id`) COMMENT '主键：记录唯一标识',
   UNIQUE KEY `service_name` (`service_name`,`build_record_name`) COMMENT '唯一索引：服务名称和构建记录名称组合唯一',
   KEY `build_record_name` (`build_record_name`) COMMENT '索引：构建记录名称',
@@ -74,11 +69,25 @@ CREATE TABLE `build_service_records` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `build_service_records`
+-- Table structure for table `cicd_tools`
 --
 
-/*!40000 ALTER TABLE `build_service_records` DISABLE KEYS */;
-/*!40000 ALTER TABLE `build_service_records` ENABLE KEYS */;
+DROP TABLE IF EXISTS `cicd_tools`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cicd_tools` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键 ID',
+  `name` varchar(100) NOT NULL COMMENT '工具名称（如 ArgoCD、Jenkins、GitLab）',
+  `type` varchar(255) NOT NULL COMMENT '工具类型',
+  `url` varchar(500) NOT NULL COMMENT '工具访问地址',
+  `credential_type` enum('token','kube_config','basic','none') DEFAULT 'none' COMMENT '认证类型',
+  `credential_name` varchar(255) DEFAULT NULL COMMENT '认证信息（如 token 或 base64 的用户名密码）',
+  `description` text COMMENT '工具描述',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='CICD 工具信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `clusters`
@@ -91,7 +100,7 @@ CREATE TABLE `clusters` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `name` varchar(255) NOT NULL COMMENT '集群名称（唯一标识）',
   `api_server` varchar(255) NOT NULL COMMENT 'K8s API Server 地址',
-  `config` text COMMENT 'Base64 或加密后的 kubeconfig 内容（可选）',
+  `config` varchar(255) NOT NULL COMMENT 'Base64 或加密后的 kubeconfig 内容（可选）',
   `region` varchar(255) DEFAULT NULL COMMENT '集群所属区域（如 cn-hz）',
   `version` varchar(255) DEFAULT NULL COMMENT 'Kubernetes 版本号',
   `description` varchar(255) DEFAULT NULL COMMENT '备注说明',
@@ -99,15 +108,8 @@ CREATE TABLE `clusters` (
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Kubernetes 集群信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Kubernetes 集群信息表';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `clusters`
---
-
-/*!40000 ALTER TABLE `clusters` DISABLE KEYS */;
-/*!40000 ALTER TABLE `clusters` ENABLE KEYS */;
 
 --
 -- Table structure for table `code_libraries`
@@ -118,47 +120,37 @@ DROP TABLE IF EXISTS `code_libraries`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `code_libraries` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '记录唯一标识，自增',
-  `name` varchar(255) DEFAULT NULL COMMENT '代码库的名称',
-  `url` varchar(255) DEFAULT NULL COMMENT '代码库地址',
-  `code_source_name` varchar(255) DEFAULT NULL COMMENT '代码源名称',
-  `project_name` varchar(255) DEFAULT NULL COMMENT '代码库所属项目',
+  `name` varchar(255) NOT NULL COMMENT '代码库的名称',
+  `url` varchar(255) NOT NULL COMMENT '代码库地址',
+  `code_source_name` varchar(255) NOT NULL COMMENT '代码源名称',
+  `project_name` varchar(255) NOT NULL COMMENT '代码库所属项目',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录最后更新时间',
   PRIMARY KEY (`id`),
   KEY `codelibrary` (`name`),
   KEY `project` (`project_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='代码库表，存储代码库的相关信息';
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='代码库表，存储代码库的相关信息';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `code_libraries`
+-- Table structure for table `credentials`
 --
 
-/*!40000 ALTER TABLE `code_libraries` DISABLE KEYS */;
-/*!40000 ALTER TABLE `code_libraries` ENABLE KEYS */;
-
---
--- Table structure for table `code_sources`
---
-
-DROP TABLE IF EXISTS `code_sources`;
+DROP TABLE IF EXISTS `credentials`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `code_sources` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL COMMENT '代码源名称',
-  `type` varchar(255) DEFAULT NULL COMMENT '代码源类型(gitlab,github)',
-  `url` varchar(255) DEFAULT NULL COMMENT '代码源地址',
-  `private_token` varchar(255) DEFAULT NULL COMMENT '代码源私钥',
+CREATE TABLE `credentials` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键 ID',
+  `name` varchar(100) NOT NULL COMMENT '凭证名称',
+  `type` enum('token','kube_config','basic','none') NOT NULL COMMENT '凭证类型',
+  `description` text COMMENT '凭证描述信息',
+  `data` text NOT NULL COMMENT '凭证内容（JSON、YAML 或加密后的字符串）',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录最后更新时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `uniq_name_type` (`name`,`type`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='统一凭证存储表';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `code_sources`
---
-
-/*!40000 ALTER TABLE `code_sources` DISABLE KEYS */;
-/*!40000 ALTER TABLE `code_sources` ENABLE KEYS */;
 
 --
 -- Table structure for table `deploy_records`
@@ -170,28 +162,23 @@ DROP TABLE IF EXISTS `deploy_records`;
 CREATE TABLE `deploy_records` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '记录唯一标识，自增',
   `name` varchar(255) NOT NULL COMMENT '发布记录名称',
-  `project_name` varchar(255) DEFAULT NULL COMMENT '发布记录所属项目',
-  `build_record_name` varchar(255) DEFAULT NULL COMMENT '发布记录所属构建记录',
-  `env` varchar(255) DEFAULT NULL COMMENT '发布记录所属环境',
+  `project_name` varchar(255) NOT NULL COMMENT '发布记录所属项目',
+  `build_record_name` varchar(255) NOT NULL COMMENT '发布记录所属构建记录',
+  `env` varchar(255) NOT NULL COMMENT '发布记录所属环境',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
   `deploy_user` varchar(255) DEFAULT NULL COMMENT '谁发布的',
-  `status` varchar(255) DEFAULT NULL COMMENT '发布状态',
-  `tag` varchar(255) DEFAULT NULL COMMENT '发布标签(测试, 预发, 上线)',
+  `status` varchar(255) NOT NULL COMMENT '发布状态',
+  `tag` varchar(255) NOT NULL COMMENT '发布标签(测试, 预发, 上线)',
   `description` text COMMENT '发布记录的描述',
-  `cluster_names` varchar(255) DEFAULT NULL COMMENT '发布到哪些集群',
+  `cluster_names` varchar(255) NOT NULL COMMENT '发布到哪些集群',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
   PRIMARY KEY (`id`) COMMENT '主键：记录唯一标识',
+  UNIQUE KEY `build_record_name_2` (`build_record_name`,`project_name`),
   KEY `build_record_name` (`build_record_name`) COMMENT '索引：构建记录名称',
   KEY `name` (`name`) COMMENT '索引：发布记录名称',
   CONSTRAINT `deploy_records_ibfk_1` FOREIGN KEY (`build_record_name`) REFERENCES `build_records` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='发布记录表，存储每次发布的相关信息';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `deploy_records`
---
-
-/*!40000 ALTER TABLE `deploy_records` DISABLE KEYS */;
-/*!40000 ALTER TABLE `deploy_records` ENABLE KEYS */;
 
 --
 -- Table structure for table `deploy_service_records`
@@ -203,25 +190,20 @@ DROP TABLE IF EXISTS `deploy_service_records`;
 CREATE TABLE `deploy_service_records` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '记录唯一标识，自增',
   `service_name` varchar(255) NOT NULL COMMENT '服务名称',
-  `project_name` varchar(255) DEFAULT NULL COMMENT '项目名称',
-  `deploy_record_name` varchar(255) DEFAULT NULL COMMENT '部署记录名称',
-  `env` varchar(255) DEFAULT NULL COMMENT '部署环境',
-  `status` varchar(255) DEFAULT NULL COMMENT '部署状态',
+  `project_name` varchar(255) NOT NULL COMMENT '项目名称',
+  `deploy_record_name` varchar(255) NOT NULL COMMENT '部署记录名称',
+  `env` varchar(255) NOT NULL COMMENT '部署环境',
+  `status` varchar(255) NOT NULL COMMENT '部署状态',
   `image` text COMMENT '服务镜像信息',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
-  `cluster_name` varchar(255) DEFAULT NULL COMMENT '集群名称',
+  `cluster_name` varchar(255) NOT NULL COMMENT '集群名称',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
   PRIMARY KEY (`id`) COMMENT '主键：记录唯一标识',
+  UNIQUE KEY `service_name` (`service_name`,`deploy_record_name`),
   KEY `deploy_record_name` (`deploy_record_name`) COMMENT '索引：部署记录名称',
   CONSTRAINT `deploy_service_records_ibfk_1` FOREIGN KEY (`deploy_record_name`) REFERENCES `deploy_records` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='部署服务记录表，存储每次部署的相关信息';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `deploy_service_records`
---
-
-/*!40000 ALTER TABLE `deploy_service_records` DISABLE KEYS */;
-/*!40000 ALTER TABLE `deploy_service_records` ENABLE KEYS */;
 
 --
 -- Table structure for table `envs`
@@ -232,23 +214,18 @@ DROP TABLE IF EXISTS `envs`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `envs` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键：环境记录唯一标识，自增',
-  `name` varchar(255) DEFAULT NULL COMMENT '环境名称',
-  `type` varchar(255) DEFAULT NULL COMMENT '环境类型',
-  `project_name` varchar(255) DEFAULT NULL COMMENT '所属项目名称',
-  `namespace` varchar(255) DEFAULT NULL COMMENT '环境的命名空间',
+  `name` varchar(255) NOT NULL COMMENT '环境名称',
+  `type` varchar(255) NOT NULL COMMENT '环境类型',
+  `project_name` varchar(255) NOT NULL COMMENT '所属项目名称',
+  `namespace` varchar(255) NOT NULL COMMENT '环境的命名空间',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`,`project_name`) COMMENT '唯一索引：环境名称和项目名称组合，确保环境名称在项目中的唯一性',
   KEY `project_name` (`project_name`) COMMENT '索引：项目名称，用于快速查找环境信息',
   CONSTRAINT `envs_ibfk_1` FOREIGN KEY (`project_name`) REFERENCES `projects` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='环境表，存储与环境相关的信息，包括环境名称、类型、所属项目和命名空间';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='环境表，存储与环境相关的信息，包括环境名称、类型、所属项目和命名空间';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `envs`
---
-
-/*!40000 ALTER TABLE `envs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `envs` ENABLE KEYS */;
 
 --
 -- Table structure for table `projects`
@@ -262,17 +239,12 @@ CREATE TABLE `projects` (
   `name` varchar(255) NOT NULL COMMENT '服务名称',
   `env` json DEFAULT NULL COMMENT '服务的环境配置信息，以 JSON 格式存储',
   `clusters` json DEFAULT NULL COMMENT '服务所属的集群信息，以 JSON 格式存储',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`) COMMENT '唯一索引：服务名称，确保服务名称唯一'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='服务表，存储与服务相关的信息，包括名称、环境配置和集群信息';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='服务表，存储与服务相关的信息，包括名称、环境配置和集群信息';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `projects`
---
-
-/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 
 --
 -- Table structure for table `services`
@@ -283,24 +255,19 @@ DROP TABLE IF EXISTS `services`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `services` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键：服务记录唯一标识，自增',
-  `name` varchar(255) DEFAULT NULL COMMENT '服务名称',
-  `project_name` varchar(255) DEFAULT NULL COMMENT '服务所属项目名称',
-  `env_name` varchar(255) DEFAULT NULL COMMENT '服务部署环境名称',
-  `code_library_name` varchar(255) DEFAULT NULL COMMENT '代码库名称',
+  `name` varchar(255) NOT NULL COMMENT '服务名称',
+  `project_name` varchar(255) NOT NULL COMMENT '服务所属项目名称',
+  `env_name` varchar(255) NOT NULL COMMENT '服务部署环境名称',
+  `code_library_name` varchar(255) NOT NULL COMMENT '代码库名称',
   `deploy_map` json DEFAULT NULL COMMENT '服务的部署映射，存储服务相关的部署信息',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`) COMMENT '唯一索引：服务名称，确保服务名称唯一',
+  UNIQUE KEY `project_name_2` (`project_name`,`name`,`env_name`),
   KEY `project_name` (`project_name`) COMMENT '索引：项目名称，用于加速查询',
   CONSTRAINT `services_ibfk_1` FOREIGN KEY (`project_name`) REFERENCES `projects` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='服务表，存储与服务相关的信息，包括名称、环境配置和集群信息';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='服务表，存储与服务相关的信息，包括名称、环境配置和集群信息';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `services`
---
-
-/*!40000 ALTER TABLE `services` DISABLE KEYS */;
-/*!40000 ALTER TABLE `services` ENABLE KEYS */;
 
 --
 -- Table structure for table `users`
@@ -312,17 +279,18 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '用户唯一标识，自增',
   `name` varchar(50) NOT NULL COMMENT '用户名，唯一',
-  `email` varchar(255) DEFAULT NULL COMMENT '用户电子邮件地址',
-  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户电话',
+  `email` varchar(255) NOT NULL COMMENT '用户电子邮件地址',
+  `phone` varchar(255) NOT NULL COMMENT '用户电话',
   `last_login` timestamp NULL DEFAULT NULL COMMENT '用户最后登录时间',
   `token` varchar(255) DEFAULT NULL COMMENT '用户登录令牌',
   `password` varchar(255) NOT NULL COMMENT '用户密码',
-  `type` varchar(255) DEFAULT NULL COMMENT '用户类型',
-  `role` varchar(255) DEFAULT NULL COMMENT '用户角色',
+  `type` varchar(255) NOT NULL COMMENT '用户类型',
+  `role` varchar(255) NOT NULL DEFAULT 'user' COMMENT '用户角色',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表，存储系统用户的相关信息';
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表，存储系统用户的相关信息';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -330,7 +298,7 @@ CREATE TABLE `users` (
 --
 
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin@neolix.cn','00000000000','2025-04-28 03:42:07','YWRtaW46aTdUSUNBaUw=','$2a$10$6zsDHgyUq2/098MNsiwMw.dlVYMeWUMjyZGgCxOnfNpJ28ANDgZsC','local','admin','2025-04-27 02:37:36'),(2,'user','user@neolix.cn','00000000000','2025-04-28 03:42:07','dXNlcjpaUlB1dm9CbQ==','$2a$10$6zsDHgyUq2/098MNsiwMw.dlVYMeWUMjyZGgCxOnfNpJ28ANDgZsC','local','user','2025-04-28 03:41:10');
+INSERT INTO `users` VALUES (1,'admin','admin@odyssey.cn','00000000000','','YWRtaW46aTdUSUNBaUw=','$2a$10$6zsDHgyUq2/098MNsiwMw.dlVYMeWUMjyZGgCxOnfNpJ28ANDgZsC','local','admin',''),(2,'user','user@odyssey.cn','00000000000','','dXNlcjpaUlB1dm9CbQ==','$2a$10$6zsDHgyUq2/098MNsiwMw.dlVYMeWUMjyZGgCxOnfNpJ28ANDgZsC','local','user','');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 --
@@ -346,4 +314,4 @@ INSERT INTO `users` VALUES (1,'admin','admin@neolix.cn','00000000000','2025-04-2
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-28 14:43:20
+-- Dump completed on 2025-04-30 14:59:24
