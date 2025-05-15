248069baaa5b1aee3c259cd8fc1b1d06a1ce060c
lihao <lihao@neolix.cn>
2025-05-15 16:37:48 +0800

    v0 ci-model

--- web/src/views/home/home.vue
+++ web/src/views/home/home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="flex-center">
+  <div class="flex flex-col items-center">
     <h1>welcome to odyssey</h1>
+    <h2>
+      GitHub: 
+      <a href="https://github.com/hozi/odyssey" target="_blank">
+        https://github.com/hozi/odyssey
+      </a>
+    </h2>
   </div>
 </template>
 <script setup lang="ts">
